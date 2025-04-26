83ff711c14c683349f4f2957b5e0b6d7b4820991
ravikant258 <ravituri258@gmail.com>
2025-04-26 21:54:03 +0530

    Add files via upload

---
+++ EmployeeDao.java
@@ -0,0 +1,29 @@
+package lms.leaveManagement.dao;
+
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface EmployeeDao {
+
+	String showAllEmployDao() throws SQLException, ClassNotFoundException;
+
+	String searchEmployDao(int empno) throws ClassNotFoundException, SQLException;
+
+	String applyLeaveDao(int empId, String DateLeaveStartDate, String LeaveEndDate, String LeaveType,
+			String LeaveReason) throws ClassNotFoundException, SQLException;
+
+	// String leaveHistory (select * from LeaveHistory where empId=1000)
+
+	String showleaveHistoryDao(int empId) throws ClassNotFoundException, SQLException;
+
+	// 3) Pending Leave History (DIsplay Managers Subordinates pending leaves
+	// history)
+
+	String showPendingLeaveHistoryDao(int mangerId) throws SQLException, ClassNotFoundException;
+
+    //	Accept/Reject (leaveId,ManagerId,Yes/No,ManagerComments)
+
+	String leaveApproveByManagerDao(int managerId) throws SQLException, ClassNotFoundException;
+
+}
